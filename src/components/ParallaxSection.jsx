0715b54a13691d4b75f40774a613ac2e2ce12b42
JakeSo <jksorrentino@gmail.com>
2025-08-07 12:21:03 -0400

    Add manifest.json, robots.txt, and sitemap.xml for improved SEO and PWA support

--- src/components/ParallaxSection.jsx
+++ src/components/ParallaxSection.jsx
@@ -1,14 +1,12 @@
 
 
 export const ParallaxSection = ({img, children}) => {
-
-
     return(
-        <div className="text-center bg-cover max-w-full section bg-no-repeat bg-center content-center min-h-fit md:h-auto md:min-h-[500px] md:bg-fixed" style={{backgroundImage: `url(${img})`}}>
+        <section className="text-center bg-cover max-w-full section bg-no-repeat bg-center content-center min-h-fit md:h-auto md:min-h-[500px] md:bg-fixed" style={{backgroundImage: `url(${img})`}}>
             <div className="bg-opacity-55 py-10 px-6 my-4 md:p-12 mx-4 md:mx-12 md:my-16 h-fit rounded-lg  bg-black  text-shadow">
                 {children}
             </div>
-      </div>
+      </section>
     )
 } 
 
