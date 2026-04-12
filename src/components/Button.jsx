8ea376b07e14b31b07c00a603f031cea0523ea74
JakeSo <jksorrentino@gmail.com>
2026-04-12 13:11:48 -0400

    de-Reacting

--- src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button = ({children, className, size, ...otherProps}) => {
-    
-    return (<button className={`rounded-full border-4 border-transparent  transition-colors duration-300 hover:border-gold text-${size} ` + className} { ...otherProps }>
-            {children}
-    </button>)
-}
-
-export default Button;
